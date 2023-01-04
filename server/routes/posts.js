d0b5f18ff090b14ebfd03480f1e2db70a170a6aa
Vishwajeet <vkchintu53@gmail.com>
2023-01-04 12:38:00 +0530

    added delete post feature

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 import react from 'react'
 import express from 'express'
 
-import {getPosts, createPost, updatePost} from '../controller/posts.js';
+import {getPosts, createPost, updatePost, deletePost} from '../controller/posts.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
 export default router; 
