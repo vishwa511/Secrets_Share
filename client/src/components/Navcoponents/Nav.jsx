46bbfb2f5ddf21ebb8f57e279ff1ad00f5a67a17
Vishwajeet <vkchintu53@gmail.com>
2023-01-29 01:16:00 +0530

    removed button

--- client/src/components/Navcoponents/Nav.jsx
+++ client/src/components/Navcoponents/Nav.jsx
@@ -7,11 +7,11 @@ function Nav(){
 			<div className='w-full p-4 text-center text-white bg-gray-600'>
 			<h1 className='text-3xl'>After all, its our memories!!</h1>
 			{/* <img className='mx-10 align-middle h-80 w-80' src = {memories} alt='memories'/> */}
-			<div class="flex spac-x-2 justify-center">
+			{/* <div class="flex spac-x-2 justify-center">
 				<Button
 					title = "Adding secret"
 					section = "#/Form"
-				/>
+				/> */}
 			{/* <div>
 				<button type="button" class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Add a Post</button>
 			</div> */}
