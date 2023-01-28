26537143fc33d2808b22708bb9c5e28bb67e681a
Vishwajeet <vkchintu53@gmail.com>
2023-01-29 01:18:28 +0530

    minor change

--- client/src/components/Navcoponents/Nav.jsx
+++ client/src/components/Navcoponents/Nav.jsx
@@ -7,8 +7,8 @@ function Nav(){
 			<div className='w-full p-4 text-center text-white bg-gray-600'>
 			<h1 className='text-3xl'>After all, its our memories!!</h1>
 			{/* <img className='mx-10 align-middle h-80 w-80' src = {memories} alt='memories'/> */}
-			{/* <div class="flex spac-x-2 justify-center">
-				<Button
+			<div class="flex spac-x-2 justify-center">
+				{/* <Button
 					title = "Adding secret"
 					section = "#/Form"
 				/> */}
