5c320276c0f978d5e1cb2d99b3e0a0d8563cb55d
Vishwajeet <vkchintu53@gmail.com>
2023-02-06 00:54:56 +0530

    added login button

--- client/src/components/Navcoponents/Button.jsx
+++ client/src/components/Navcoponents/Button.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { HashLink as Link } from "react-router-hash-link";
 
-function Button(title, section){
+function Button(props){
 	return(
-		<div>
+		<div className="p-4">
 			{/* <Link to={section}> */}
 			<div>
-				<button type="button" class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Add a Post</button>
+				<button type="button" class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">{props.value}</button>
 			</div>
 			{/* </Link> */}
 		</div>
