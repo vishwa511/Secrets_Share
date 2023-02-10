bc101ac795bf144d23cea5f0a7fc913c796f5c25
Vishwajeet <vkchintu53@gmail.com>
2023-02-10 11:46:05 +0530

    minor additions in login page

--- client/src/components/Auth/Login/Login.jsx
+++ client/src/components/Auth/Login/Login.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+import jwt_decode from 'jwt-decode';
+import {useDispatch} from 'react-redux'
+import { useNavigate } from 'react-router-dom';
 
 
 function Login(props){
+
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const googleSuccess = async(res) =>{
+	const result = res;
+	const token  = res?.jti;
+		
+		console.log(result);
+		console.log(token);
+		
+		try {
+			dispatch({type: 'AUTH', data: {result, token}})
+			navigate('/');
+		} catch (error) {
+			console.log(error);
+		}				
+	}
+
 	return(
 		<div>
 
@@ -39,11 +62,25 @@ function Login(props){
                   </div>
                   <button type="submit" className="w-full text-white bg-black focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
 
+				  <GoogleOAuthProvider clientId="938565639590-rl20oaugdsipvf4vl5avnecughhf6bm9.apps.googleusercontent.com">
+				  <GoogleLogin
+						onSuccess={credentialResponse => {
+							// console.log(credentialResponse);
+							const details = jwt_decode(credentialResponse.credential);
 
-					<button type="button" class="w-full justify-center text-white bg-[#e22d2dd3] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
+							googleSuccess(details);
+							
+						}}
+						onError={() => {
+							console.log('Login Failed');
+						}}
+						/>
+					</GoogleOAuthProvider>
+
+					{/* <button type="button" class="w-full justify-center text-white bg-[#e22d2dd3] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
 					<svg class="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
 					Sign in with Google
-					</button>
+					</button> */}
 
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Don't have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500" onClick ={props.onChanging}>Sign up</a>
